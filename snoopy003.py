4f8be5e60757f2f31fc0f0d38ec7282c0cad3e27
hnk0718 <94042853+hnk0718@users.noreply.github.com>
2021-11-10 18:06:58 +0900

    Create snoopy003.py

---
+++ snoopy003.py
@@ -0,0 +1,18 @@
+#Pythonの内容を書く
+from anime2021.anime import AShape, RollingPolygon, AImage
+
+class GuruGuruNami(AShape):
+  def __init__(self, width=100, height=None, cx=None, cy=None, N=3):
+    AShape.__init__(self, width, height, cx, cy)
+    self.N = N
+    self.rolling = RollingPolygon(width, height, cx, cy, N)
+    self.image = AImage(width, height, cx, cy)
+
+  def render(self, canvas, tick):
+    # 描画する前に中心を同期する
+    self.rolling.cx = self.cx
+    self.rolling.cy = self.cy
+    self.image.cx = self.cx
+    self.image.cy = self.cy
+    self.rolling.render(canvas, tick)
+    self.image.render(canvas, tick)
