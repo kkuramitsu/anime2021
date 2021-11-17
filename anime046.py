a9c1a6d66025f763e78af9fa0930735dd64bbfe4
Mi-mimi <94042803+Mi-mimi@users.noreply.github.com>
2021-11-17 15:10:07 +0900

    Create anime046.py

---
+++ anime046.py
@@ -0,0 +1,15 @@
+from anime2021.anime import AShape, RollingPolygon, AImage
+
+class GuruGurusinamon(AShape):
+  def __init__(self,width=50, height=None, cx=None, cy=None, N=8):
+    AShape.__init__(self, width, height, cx, cy)
+    self.poly = RollingPolygon(width, height, N=N)
+    self.sinamon=AImage(width, height, image=sinamon)
+
+  def render(self, canvas, tick):
+    self.poly.cx=self.cx
+    self.poly.cy=self.cy
+    self.sinamon.cx=self.cx
+    self.sinamon.cy=self.cy
+    self.poly.render(canvas, tick)
+    self.sinamon.render(canvas, tick)
