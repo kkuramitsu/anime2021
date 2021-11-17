3826eee8781bf2cca1cc4426f471caa5161dbaaa
HonamiMachida <94042832+HonamiMachida@users.noreply.github.com>
2021-11-17 17:42:13 +0900

    Update Anpanman072.py

--- Anpanman072.py
+++ Anpanman072.py
@@ -1,11 +1,9 @@
 from anime2021.anime import AShape,test_shape,ACanvas,Image
-
-!wget https://pics.prcm.jp/64eda603621c9/82347972/png/82347972.png
-
 class RollingAnpanman(AShape):
     color: any
+    Anpanman='82347972.png'
 
-    def __init__(self, width=100, height=None, cx=None, cy=None, image='82347972.png'):
+    def __init__(self, width=100, height=None, cx=None, cy=None, image=Anpanman):
         AShape.__init__(self, width, height, cx, cy)
         if image.startswith('https://pics.prcm.jp/64eda603621c9/82347972/png/82347972.png'):
             self.pic = Image.open(io.BytesIO(requests.get(image).content))
@@ -20,4 +18,3 @@ class RollingAnpanman(AShape):
         canvas.image.paste(pic, (int(ox+100), int(oy)), pic)
         canvas.image.paste(pic, (int(ox), int(oy+100)), pic)
         canvas.image.paste(pic, (int(ox), int(oy-100)), pic)
-
