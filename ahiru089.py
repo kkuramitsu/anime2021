58e4f9394cb6b903c2080355c6e17e72e7c626e1
miharuY <94042860+miharuY@users.noreply.github.com>
2021-11-30 10:24:14 +0900

    Create ahiru089.py

---
+++ ahiru089.py
@@ -0,0 +1,5 @@
+from anime2021.anime import ,AShape, AStudio, AImage ,test_shape
+
+!wget https://3.bp.blogspot.com/-9i7uW0JycJc/UZmCRl25KCI/AAAAAAAATfc/5uBk-6YxV_Q/s800/furo_ducky.png
+
+IPython.display.Image(test_shape(AImage(image='furo_ducky.png')))
