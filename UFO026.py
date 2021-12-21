7f1760b031a3a88daa6a89f9306ebcfe8b9082cb
yff58507 <94023016+yff58507@users.noreply.github.com>
2021-12-21 11:04:30 +0900

    Update UFO026.py

--- UFO026.py
+++ UFO026.py
@@ -1,30 +1,34 @@
 from anime2021.anime import ACanvas, AShape, AStudio, ARectangle
 
 
-!wget https://1.bp.blogspot.com/-8vjnbp_AXMM/USSkq9AIKTI/AAAAAAAANV0/PQ6FLf-xUks/s1600/alien_ufo.png
+alien_ufo = 'https://1.bp.blogspot.com/-8vjnbp_AXMM/USSkq9AIKTI/AAAAAAAANV0/PQ6FLf-xUks/s1600/alien_ufo.png'
 
-!wget https://2.bp.blogspot.com/-A0Rk-ZNi4OI/VXOUSDVDoAI/AAAAAAAAuJU/4NngWeOUZW4/s400/hawaii_maunakea.png
+hawaii_maunakea = 'https://2.bp.blogspot.com/-A0Rk-ZNi4OI/VXOUSDVDoAI/AAAAAAAAuJU/4NngWeOUZW4/s400/hawaii_maunakea.png'
 
 import math
 
 class AUFO(AShape):
     color: any
 
-    def __init__(self, width=100, height=None, cx=None, cy=None, image='alien_ufo.png'):
+    def __init__(self, width=100, height=None, cx=None, cy=None, image=alien_ufo):
         AShape.__init__(self, width, height, cx, cy)
-        self.pic = Image.open(image)
+        if image.startswith('http'):
+            self.pic = Image.open(io.BytesIO(requests.get(image).content))
+        else:
+            self.pic = Image.open(image)
 
     def render(self, canvas: ACanvas, frame: int):
         ox, oy, w, h = self.bounds()
         pic = self.pic.resize((int(w), int(h)))
         canvas.image.paste(pic, (int(ox), int(oy)), pic)
+        
 
     def FlyingUFO(shape, A=100, B=100, a=1, b=1, delta=0):
         # スタジオを用意
         studio = AStudio(400,300)
 
         # スタジオに背景を追加する
-        studio.append(AUFO(width=400, height=300, image='hawaii_maunakea.png'))    
+        studio.append(AUFO(width=400, height=300, image=hawaii_maunakea))   
 
         # スタジオにUFOを追加する
         studio.append(shape)
