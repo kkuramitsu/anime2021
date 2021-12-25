a4362a635a9d34f85c5a31c04230a84ca38436c5
Tunanatu <94043480+Tunanatu@users.noreply.github.com>
2021-12-25 20:27:12 +0900

    Update anime024.py

--- anime024.py
+++ anime024.py
@@ -156,12 +156,13 @@ class AText(AShape):
         canvas.draw.text((self.cx, self.cy), str(self.data), fill=self.color)
 
 import math
-studio = AStudio()
-shape =  RollingPolygon(width=200, N=5, color='orange')
-studio.append(shape)
-studio.append(RollingPolygon(width=200, N=4, color='beige'))
-studio.append(RollingPolygon(width=200, N=3, color='pink'))
-studio.append(AText(width=170, data="spin", color='red'))
-for i in range(60):
+class guruguru3():
+    studio = AStudio()
+    shape =  RollingPolygon(width=200, N=5, color='orange')
+    studio.append(shape)
+    studio.append(RollingPolygon(width=200, N=4, color='beige'))
+    studio.append(RollingPolygon(width=200, N=3, color='pink'))
+    studio.append(AText(width=170, data="spin", color='red'))
+    for i in range(60):
     studio.render()
-IPython.display.Image(studio.create_anime())
+    IPython.display.Image(studio.create_anime())
