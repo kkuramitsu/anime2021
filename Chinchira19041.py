eadb29c042bc1420a1acd307e8cdf0add8f736be
fujisaki-tamago <79297107+fujisaki-tamago@users.noreply.github.com>
2021-12-27 16:01:46 +0900

    Update Chinchira19041.py

--- Chinchira19041.py
+++ Chinchira19041.py
@@ -1,32 +1,23 @@
-!wget https://1.bp.blogspot.com/-OBrXImM0Sd4/X5OcFkxjEnI/AAAAAAABb40/HSDcWu_nYSUbAmRlWro9bHF6pkZJEyFngCNcBGAsYHQ/s789/animal_mawashi_guruma_chinchira.png
-!git clone https://github.com/kkuramitsu/anime2021.git
-from anime2021.anime import AShape, AStudio, ACanvas, AImage
-!pip install apng
-import os, IPython
-from PIL import Image, ImageDraw, ImageFont
-from apng import APNG
+from anime2021.anime import *
 
 import math
-
-def chinchira_shape(shape):
-    studio = AStudio(400,300)
-
-    studio.append(shape)
-
-    frames = 50
-    for t in range(frames):      
-        x = 400 - 8*t
-        y = 150
-        shape.cx = x
-        shape.cy = y
-        studio.render()
-    return studio.create_anime(delay=50)
   
 class RollingChinchira(AImage):
+    
+    def __init__(self, width=100, height=None, cx=None, cy=None):
+        self.Chinchira_image = AImage(width, height, image=Chinchira_image)
+        
+    def chinchira_shape(test_shape):
+        frames = 50
+        for t in range(frames):      
+            x = 400 - 8*t
+            y = 150
+            shape.cx = x
+            shape.cy = y
+            studio.render()
+        return studio.create_anime(delay=50)
 
     def render(self, canvas: ACanvas, tick: int):
-        ox, oy, w, h = self.bounds()
-        pic = self.pic.resize((int(w), int(h)))
         r = min(self.width, self.height)/2
         for i in range(180):
             slope = math.pi * 2 * 10 * (tick/180)
@@ -34,4 +25,4 @@ class RollingChinchira(AImage):
             oy = self.cy + r * math.sin(i * slope)
         canvas.image.paste(pic, (int(ox), int(oy)), pic)
     
-IPython.display.Image(chinchira_shape(RollingChinchira()))
+IPython.display.Image(RollingChinchira(shape))
