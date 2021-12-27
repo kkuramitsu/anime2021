97528256b23bc452f74c3f525f25ef565736a757
fujisaki-tamago <79297107+fujisaki-tamago@users.noreply.github.com>
2021-12-27 18:04:34 +0900

    Update Chinchira19041.py

--- Chinchira19041.py
+++ Chinchira19041.py
@@ -1,28 +1,34 @@
 from anime2021.anime import *
+chinchira_url = 'https://1.bp.blogspot.com/-OBrXImM0Sd4/X5OcFkxjEnI/AAAAAAABb40/HSDcWu_nYSUbAmRlWro9bHF6pkZJEyFngCNcBGAsYHQ/s789/animal_mawashi_guruma_chinchira.png'
 
-import math
-  
 class RollingChinchira(AImage):
-    
-    def __init__(self, width=100, height=None, cx=None, cy=None):
-        self.Chinchira_image = AImage(width, height, image=Chinchira_image)
-        
-    def chinchira_shape(test_shape):
-        frames = 50
-        for t in range(frames):      
-            x = 400 - 8*t
-            y = 150
-            shape.cx = x
-            shape.cy = y
-            studio.render()
-        return studio.create_anime(delay=50)
+    def __init__(self, width=100, height=100, cx=None, cy=None, image=chinchira_url, scale=1.0):
+      self.width = width
+      self.height = height
+      self.scale = scale
+      if image.startswith('http'):
+          self.pic = Image.open(io.BytesIO(requests.get(image).content))
+      else:
+          self.pic = Image.open(image)
 
     def render(self, canvas: ACanvas, tick: int):
+        ox, oy, w, h = self.bounds()
+        pic = self.pic.resize((int(w), int(h)))
         r = min(self.width, self.height)/2
         for i in range(180):
             slope = math.pi * 2 * 10 * (tick/180)
             ox = self.cx + r * math.cos(i * slope)
             oy = self.cy + r * math.sin(i * slope)
         canvas.image.paste(pic, (int(ox), int(oy)), pic)
-    
-IPython.display.Image(RollingChinchira(shape))
+
+def chinchira_shape(shape):
+    studio = AStudio(400,300)
+    studio.append(shape)
+    frames = 50
+    for t in range(frames):      
+        x = 400 - 8*t
+        y = 150
+        shape.cx = x
+        shape.cy = y
+        studio.render()
+    return studio.create_anime(delay=50)
