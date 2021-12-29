7a73b5e43d9aaf9748745ecddf410091125b3342
kurimpan <94042798+kurimpan@users.noreply.github.com>
2021-12-30 00:48:00 +0900

    Update GuruGuruSanta013.py

--- GuruGuruSanta013.py
+++ GuruGuruSanta013.py
@@ -1,13 +1,13 @@
 import os, IPython
-from anime2021.anime import AStudio,AShape,test_shape,ACanvas,Image
+from PIL import Image, ImageDraw, ImageFont
+font = ImageFont.truetype('/usr/share/fonts/truetype/fonts-japanese-gothic.ttf', 40)
+import math
+from anime2021.anime import AStudio, AShape,  test_shape, ACanvas
 
-small_star7_yellow = 'https://2.bp.blogspot.com/-iLvMf6s3b6M/V0QnoQqvVzI/AAAAAAAA69w/AdU5l6HIy3MX5CTXrfbMNNLbuksyyoC3QCLcB/s800/small_star7_yellow.png'
-christmas_santa_sori = 'https://1.bp.blogspot.com/-8FaGc_NoAiU/UYzXHqVasJI/AAAAAAAAR4k/540GCCzmI38/s500/christmas_santa_sori.png'
-  
 class AImage(AShape):
     color: any
 
-    def __init__(self, width=100, height=None, cx=None, cy=None, image = small_star7_yellow):
+    def __init__(self, width=100, height=None, cx=None, cy=None, image='small_star7_yellow.png'):
         AShape.__init__(self, width, height, cx, cy)
         if image.startswith('http'):
             self.pic = Image.open(io.BytesIO(requests.get(image).content))
@@ -18,11 +18,10 @@ class AImage(AShape):
         ox, oy, w, h = self.bounds()
         pic = self.pic.resize((int(w), int(h)))
         canvas.image.paste(pic, (int(ox), int(oy)), pic)
-        
 class AImage2(AShape):
     color: any
 
-    def __init__(self, width=100, height=None, cx=None, cy=None, image = christmas_santa_sori):
+    def __init__(self, width=100, height=None, cx=None, cy=None, image='christmas_santa_sori.png'):
         AShape.__init__(self, width, height, cx, cy)
         if image.startswith('http'):
             self.pic = Image.open(io.BytesIO(requests.get(image).content))
@@ -33,8 +32,7 @@ class AImage2(AShape):
         ox, oy, w, h = self.bounds()
         pic = self.pic.resize((int(w), int(h)))
         canvas.image.paste(pic, (int(ox), int(oy)), pic)
-        
-class GuruGuruSanta(AShape):
+ class GuruGuruSanta(AShape):
   def __init__(self, width=100, height=None, cx=None, cy=None, N=3):
     AShape.__init__(self, width, height, cx, cy)
     self.N = N
@@ -47,4 +45,4 @@ class GuruGuruSanta(AShape):
     self.image2.cy = self.cy
     self.image.render(canvas, tick)  
     self.image2.render(canvas, tick) 
-    
+ 
