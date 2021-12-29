25b46bf38acbc558e027ec2a002ce4a0c1181055
shizufleur <82163185+shizufleur@users.noreply.github.com>
2021-12-29 22:00:24 +0900

    Create shoukan054.py

---
+++ shoukan054.py
@@ -0,0 +1,20 @@
+from anime2021.anime import AShape, AStudio, RollingPolygon, AImage, test_shape
+import UPython
+
+syoukan='https://1.bp.blogspot.com/-P7kfu7ps8cQ/XhwqQ_oKFeI/AAAAAAABW9g/dHjpNuVa-AsOY0fKxY5h_ZHtaymlJnRrwCNcBGAsYHQ/s400/fantasy_mahoujin_syoukan.png'
+shape = AImage(100, 100, image=syoukan)
+IPython.display.Image(test_shape(shape))
+
+studio = AStudio()
+shape = RollingPolygon(width=160, N=5,color='purple')
+studio.append(shape)
+shape = RollingPolygon(width=160, N=5,slope=2,color='blue')
+studio.append(shape)
+shape = RollingPolygon(width=150, N=4,color='green')
+studio.append(shape)
+shape = AImage(150, 150, image=syoukan)
+studio.append(shape)
+
+for i in range(60):
+    studio.render()
+IPython.display.Image(studio.create_anime())
