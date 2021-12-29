a48e4059cdc4825067f8959e41424f0fd0bd08b7
shizufleur <82163185+shizufleur@users.noreply.github.com>
2021-12-29 22:29:07 +0900

    Update shoukan054.py

--- shoukan054.py
+++ shoukan054.py
@@ -1,7 +1,7 @@
 from anime2021.anime import AShape, AStudio, RollingPolygon, AImage, test_shape
 import IPython
 
-syoukan='https://1.bp.blogspot.com/-P7kfu7ps8cQ/XhwqQ_oKFeI/AAAAAAABW9g/dHjpNuVa-AsOY0fKxY5h_ZHtaymlJnRrwCNcBGAsYHQ/s400/fantasy_mahoujin_syoukan.png'
+shokan='https://1.bp.blogspot.com/-P7kfu7ps8cQ/XhwqQ_oKFeI/AAAAAAABW9g/dHjpNuVa-AsOY0fKxY5h_ZHtaymlJnRrwCNcBGAsYHQ/s400/fantasy_mahoujin_syoukan.png'
 
 studio = AStudio()
 shape = RollingPolygon(width=160, N=5,color='purple')
@@ -10,7 +10,7 @@ shape = RollingPolygon(width=160, N=5,slope=2,color='blue')
 studio.append(shape)
 shape = RollingPolygon(width=150, N=4,color='green')
 studio.append(shape)
-shape = AImage(150, 150, image=syoukan)
+shape = AImage(150, 150, image=shokan)
 studio.append(shape)
 
 for i in range(60):
