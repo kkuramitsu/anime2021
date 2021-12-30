17d3af4eb98c35ea33051e439e38e7259efa084c
kurimpan <94042798+kurimpan@users.noreply.github.com>
2021-12-30 23:04:44 +0900

    Update GuruGuruSanta013.py

--- GuruGuruSanta013.py
+++ GuruGuruSanta013.py
@@ -1,4 +1,8 @@
-from anime2021.anime import *
+from anime2021.anime import ACanvas, AShape, AStudio, RollingPolygon, AImage, test_shape
+import math
+import os, IPython, io, requests
+from PIL import Image, ImageDraw, ImageFont
+from apng import APNG
 
 small_star7_yellow = 'https://1.bp.blogspot.com/-8FaGc_NoAiU/UYzXHqVasJI/AAAAAAAAR4k/540GCCzmI38/s500/christmas_santa_sori.png'
 christmas_santa_sori = 'https://2.bp.blogspot.com/-iLvMf6s3b6M/V0QnoQqvVzI/AAAAAAAA69w/AdU5l6HIy3MX5CTXrfbMNNLbuksyyoC3QCLcB/s800/small_star7_yellow.png'
