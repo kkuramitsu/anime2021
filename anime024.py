564f5cd11f03aa39c93c44fa8cf9fb71e55fe8c6
Tunanatu <94043480+Tunanatu@users.noreply.github.com>
2021-12-30 18:05:13 +0900

    Update anime024.py

--- anime024.py
+++ anime024.py
@@ -1,11 +1,13 @@
-class guruguru3():
-    import math
-    studio = AStudio()
-    shape =  RollingPolygon(width=200, N=5, color='orange')
-    studio.append(shape)
-    studio.append(RollingPolygon(width=200, N=4, color='beige'))
-    studio.append(RollingPolygon(width=200, N=3, color='pink'))
-    studio.append(AText(width=170, data="spin", color='red'))
-    for i in range(60):
+from anime2021.anime import AStudio, RollingPolygon, AShape, AText, test_shape
+import IPython
+
+
+studio = AStudio()
+shape =  RollingPolygon(width=200, N=5, color='orange')
+studio.append(shape)
+studio.append(RollingPolygon(width=200, N=4, color='beige'))
+studio.append(RollingPolygon(width=200, N=3, color='pink'))
+studio.append(AText(width=170, data="spin", color='red'))
+for i in range(60):
     studio.render()
-   
+IPython.display.Image(studio.create_anime())
