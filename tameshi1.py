124df9f3c0953b93b74f9bea9f43f1e91faa7f4f
Tunanatu <94043480+Tunanatu@users.noreply.github.com>
2021-12-30 18:04:44 +0900

    Rename anime024.py to tameshi1.py

--- tameshi1.py
+++ tameshi1.py
@@ -1,14 +1,13 @@
-class ire:
-    from anime2021.anime import AStudio, RollingPolygon, AShape, AText, test_shape
-    import IPython
+from anime2021.anime import AStudio, RollingPolygon, AShape, AText, test_shape
+import IPython
 
-class guru:
-  studio = AStudio()
-  shape =  RollingPolygon(width=200, N=5, color='orange')
-  studio.append(shape)
-  studio.append(RollingPolygon(width=200, N=4, color='beige'))
-  studio.append(RollingPolygon(width=200, N=3, color='pink'))
-  studio.append(AText(width=170, data="spin", color='red'))
-  for i in range(60):
+
+studio = AStudio()
+shape =  RollingPolygon(width=200, N=5, color='orange')
+studio.append(shape)
+studio.append(RollingPolygon(width=200, N=4, color='beige'))
+studio.append(RollingPolygon(width=200, N=3, color='pink'))
+studio.append(AText(width=170, data="spin", color='red'))
+for i in range(60):
     studio.render()
-  IPython.display.Image(studio.create_anime())
+IPython.display.Image(studio.create_anime())
